6d2c46fdb1242b336a058dd6afdb4d9b6893697b
Brecht Omey <brecht@brec.ht>
2014-10-09 10:18:05 +0200

    changed structure: each locale now has a folder, countries file, continents file and a regions folder

---
+++ fr/continents.js
@@ -0,0 +1,16 @@
+/*
+ * @summary Continents
+ */
+var continents = {
+
+  "AF": "Afrique",
+  "AN": "Antarctique",
+  "AS": "Asie",
+  "EU": "Europe",
+  "NA": "Amérique du Nord",
+  "OC": "Océanie",
+  "SA": "Amérique du Sud"
+
+};
+
+module.exports = continents;
